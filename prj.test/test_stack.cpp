1ab59f11b06be08c5210ac87bbe5c9623921d914
choseenonee <okaymans44@gmail.com>
2024-02-12 11:37:55 +0300

    add: stack operator= test

--- prj.test/test_stack.cpp
+++ prj.test/test_stack.cpp
@@ -41,7 +41,7 @@ TEST_CASE("stack pop exception") {
     CHECK_THROWS(stack.Top());
 }
 
-TEST_CASE("stackarr test") {
+TEST_CASE("Stack test") {
     Stack my_stack;
     CHECK_THROWS(my_stack.Top());
     CHECK(my_stack.IsEmpty());
@@ -74,4 +74,57 @@ TEST_CASE("stackarr test") {
     CHECK_EQ(my_stack.Top(), fourth_complex);
     my_stack.Push(fifth_complex);
     CHECK_EQ(my_stack.Top(), fifth_complex);
+}
+
+TEST_CASE("Stack test") {
+    Stack my_stack;
+    CHECK_THROWS(my_stack.Top());
+    CHECK(my_stack.IsEmpty());
+
+    Complex first_complex(3, 5);
+    Complex second_complex(2, 7);
+
+    my_stack.Push(first_complex);
+    my_stack.Push(second_complex);
+
+    CHECK_EQ(my_stack.Top(), second_complex);
+    my_stack.Pop();
+    CHECK_EQ(my_stack.Top(), first_complex);
+
+    Complex third_complex(1, 1);
+
+    Stack my_stack2;
+    my_stack2.Push(third_complex);
+    my_stack = my_stack2;
+    CHECK_EQ(my_stack.Top(), third_complex);
+
+    Stack a(my_stack);
+    CHECK_EQ(a.Top(), third_complex);
+
+    Complex fourth_complex(2.3, 7.3);
+
+    Complex fifth_complex(2.003, 0);
+
+    my_stack.Push(fourth_complex);
+    CHECK_EQ(my_stack.Top(), fourth_complex);
+    my_stack.Push(fifth_complex);
+    CHECK_EQ(my_stack.Top(), fifth_complex);
+
+    Stack eq1;
+    Stack eq2;
+    eq1.Push(first_complex);
+    eq1.Push(second_complex);
+    eq1.Push(third_complex);
+    eq2 = eq1;
+    CHECK_EQ(eq1.Top(), eq2.Top());
+    eq1.Pop();
+    eq2.Pop();
+    CHECK_EQ(eq1.Top(), eq2.Top());
+    eq1.Pop();
+    eq2.Pop();
+    CHECK_EQ(eq1.Top(), eq2.Top());
+    eq1.Pop();
+    eq2.Pop();
+    CHECK(eq1.IsEmpty());
+    CHECK(eq2.IsEmpty());
 }
