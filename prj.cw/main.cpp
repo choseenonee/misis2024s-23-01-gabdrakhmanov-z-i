0732eb36e59f95bce2809cddf7c2ac658817cf37
choseenonee <okaymans44@gmail.com>
2024-03-29 15:53:38 +0300

    upd cw: displays window with derivatives, blue as x, green as y axis

--- prj.cw/main.cpp
+++ prj.cw/main.cpp
@@ -1,22 +1,9 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 #include <iostream>
+
+
 using namespace cv;
-//Mat src, src_gray;
-//Mat dst, detected_edges;
-//int lowThreshold = 0;
-//const int max_lowThreshold = 100;
-//const int ratio = 3;
-//const int kernel_size = 3;
-//const char* window_name = "Edge Map";
-//static void CannyThreshold(int, void*)
-//{
-//    blur( src_gray, detected_edges, Size(12,12) );
-//    Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
-//    dst = Scalar::all(0);
-//    src.copyTo( dst, detected_edges);
-//    imshow( window_name, dst );
-//}
 
 
 int main()
@@ -61,9 +48,6 @@ int main()
     auto channels = std::vector<Mat>{show_dst_x, show_dst_y, show_dst_zero};
 
     merge(channels, output);
-//    imshow("Display window", show_dst_x);
-//
-//    waitKey(0);
 
     imshow("Display window", output);
 
