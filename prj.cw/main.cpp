d783dedf12938f5b07de12d87fbd61f4cd09f5f9
choseenonee <okaymans44@gmail.com>
2024-03-29 15:42:27 +0300

    upd cw: on read displays channelds and depth of input image

--- prj.cw/main.cpp
+++ prj.cw/main.cpp
@@ -26,31 +26,31 @@ int main()
     if( src.empty() )
     {
         std::cout << "Could not open or find the image!\n" << std::endl;
-//        std::cout << "Usage: " << argv[0] << " <Input image>" << std::endl;
         return -1;
     }
-//    dst.create( src.size(), src.type() );
+
+    std::cout << "channels: " << src.channels() << std::endl << "depth: "; // CV_8UC3
+    switch (src.depth()) {
+        case CV_16U:
+            std::cout << "16 bit unsigned";
+            break;
+        case CV_8U:
+            std::cout << "8 bit unsigned";
+            break;
+    };
+
     cvtColor( src, src_gray, COLOR_BGR2GRAY );
-//    namedWindow( window_name, WINDOW_AUTOSIZE );
-//    createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
-//    CannyThreshold(0, 0);
-
-//    cv::imshow("Display window", src_gray);
-
-//    cv::imshow("Display window", dst);
-
-//    waitKey(0);
 
     cv::Mat dst;
 
     Mat show_dst_x;
     Mat show_dst_y;
 
-    cv::Sobel(src_gray, dst, CV_16S, 1, 0, 3);
-    dst.convertTo(show_dst_x, CV_8UC1, 0.5, 127);
+    cv::Sobel(src_gray, dst, CV_8U, 1, 0, 3);
+    dst.convertTo(show_dst_x, CV_8UC1, 1, 0);
 
-    cv::Sobel(src_gray, dst, CV_16S, 0, 1, 3);
-    dst.convertTo(show_dst_y, CV_8UC1, 0.5, 127);
+    cv::Sobel(src_gray, dst, CV_8U, 0, 1, 3);
+    dst.convertTo(show_dst_y, CV_8UC1, 1, 0);
 
     imshow("Display window", show_dst_x);
 
